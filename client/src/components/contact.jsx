19042e74fc263b6dc72a36ecf2b6250be94d62a7
Jonathan McNamara <jpmcnmr@gmail.com>
2019-03-21 06:12:34 -0700

    added text to contact component, changed font color to silver-grey

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 import styles from '../css/contact.css';
 const Contact = (props) => {
   return (
-    <div className={styles.main}>contact info</div>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <span>Other Info</span>
+        <div className={styles.info}>
+          <span>From: Bakersfield, California</span>
+          <span>Age: 31</span>
+          <span>Education:
+            <img></img>
+            <img></img>
+          </span>
+          <span>
+            Places lived:
+            Bakersfield, CA
+            Davis, CA
+            San Francisco, CA
+            Seongju, South Korea
+            Busan, South Korea
+            Katsuura, Tokushima-ken, Japan
+          </span>
+          <a>
+            Find me on linked in!
+          </a>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
